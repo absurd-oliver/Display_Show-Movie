2cd7978ee57eff5c7e9287f9c8dcd7584a8dbe8b
absurdoliver <166875516+absurd-oliver@users.noreply.github.com>
2025-09-15 22:27:26 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function start() {
+  setTimeout(() => {
+    window.location.href = 'https://absurd-oliver.github.io/landingPageProject/pages/landing/landing.html';
+}, 1000);
+}
+window.addEventListener("DOMContentLoaded", () => {start();});
