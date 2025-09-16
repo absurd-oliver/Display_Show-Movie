fb0f9b0a9250cd591e2af03c43492f16c8812a1e
absurdoliver <166875516+absurd-oliver@users.noreply.github.com>
2025-09-15 22:30:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function start() {
   setTimeout(() => {
-    window.location.href = 'https://absurd-oliver.github.io/landingPageProject/pages/landing/landing.html';
+    window.location.href = 'https://absurd-oliver.github.io/TLMP/';
 }, 1000);
 }
 window.addEventListener("DOMContentLoaded", () => {start();});
